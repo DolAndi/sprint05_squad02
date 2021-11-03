8f8b88c76420f8a90bb37c2420cf6e38e8906bc5
Andriano <anditoazza@gmail.com>
2021-11-03 17:06:16 -0300

    rota /login

--- cypress/integration/serveRestLogin.spec.js
+++ cypress/integration/serveRestLogin.spec.js
@@ -1,5 +1,52 @@
 /// <reference types="cypress" />
 
 import Factory from "../dynamics/factory.js"
-
 var bearer
+
+describe("Testes da rota /login", () => {
+    describe("Deve efetuar o teste positivo", () => {
+        it("Deve validar o login com status code 200 e authorization", () => {
+            cy.fixture("loginCredentials").then((user) => {
+                cy.logar(user.valido).then( res => {
+                    expect(res.status).to.equal(200)
+                    expect(res.body).to.have.property("authorization")
+                    bearer = res.body.authorization
+                })
+            })
+        })
+    })
+    describe("Deve efetuar testes negativos", () => {
+        it("Deve efetuar uma falha em que o campo email não pode ficar em branco e status code 400", () => {
+            cy.fixture("loginCredentials").then((user) => {
+                cy.logar(user.emailEmBranco).then(res => {
+                    expect(res.status).to.be.equal(400);
+                    expect(res.body).has.property("email").to.be.equal("email não pode ficar em branco")
+                })
+            })
+        })
+        it("Deve efetuar uma falha em que o campo email é obrigatório e status code 400", () => {
+            cy.fixture("loginCredentials").then((user) => {
+                cy.logar(user.semCampoEmail).then(res => {
+                    expect(res.status).to.be.equal(400);
+                    expect(res.body).has.property("email").to.be.equal("email é obrigatório")
+                })
+            })
+        })
+        it("Deve efetuar uma falha em que o campo senha(password) não pode ficar em branco e status code 400", () => {
+            cy.fixture("loginCredentials").then((user) => {
+                cy.logar(user.senhaEmBranco).then(res => {
+                    expect(res.status).to.be.equal(400);
+                    expect(res.body).has.property('password').to.be.equal("password não pode ficar em branco")
+                })
+            })
+        })
+        it("Deve efetuar uma falha em que o campo senha(password) é obrigatório e status code 400", () => {
+            cy.fixture("loginCredentials").then((user) => {
+                cy.logar(user.semCampoSenha).then(res => {
+                    expect(res.status).to.be.equal(400);
+                    expect(res.body).has.property("password").to.be.equal("password é obrigatório")
+                })
+            })
+        })
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,4 +3,11 @@
 import Ajv from "ajv"
 const ajv = new Ajv({allErrors: true, verbose: true, strict: false})
 
-
+Cypress.Commands.add("logar", usuario => {
+    return cy.request({
+        method: "POST",
+        url: `${Cypress.env("base_url")}/login`,
+        failOnStatusCode: false,
+        body: usuario
+    })
+})
